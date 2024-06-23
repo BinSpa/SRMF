dce46ef12392ec45c364e4446c7e0823f0351ba5
BinSpa <luogu1024116558@gmail.com>
2024-06-23 06:54:02 +0000

    deeplabv3plus change configs

--- configs/deeplabv3plus/deeplabv3plus_r101-d8_4xb4-160k_urur-512x512.py
+++ configs/deeplabv3plus/deeplabv3plus_r101-d8_4xb4-160k_urur-512x512.py
@@ -14,6 +14,10 @@ model = dict(
     backbone=dict(depth=101),
     test_cfg = dict(mode='slide',crop_size=(512, 512),  stride=(341, 341)))
 
+train_dataloader = dict(batch_size=8, num_workers=10)
+test_dataloader = dict(batch_size=2, num_workers=4)
+val_dataloader = test_dataloader
+
 train_cfg = dict(
     type='IterBasedTrainLoop', max_iters=160000, val_interval=2000)
 
