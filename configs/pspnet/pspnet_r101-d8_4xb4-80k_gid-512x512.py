c83351cf4303e9f3574a90506d0a885de278bfc6
BinSpa <luogu1024116558@gmail.com>
2024-08-28 06:30:19 +0000

    changes

--- configs/pspnet/pspnet_r101-d8_4xb4-80k_gid-512x512.py
+++ configs/pspnet/pspnet_r101-d8_4xb4-80k_gid-512x512.py
@@ -9,4 +9,20 @@ model = dict(
     backbone=dict(depth=101),
     data_preprocessor=data_preprocessor,
     decode_head=dict(num_classes=6),
-    auxiliary_head=dict(num_classes=6))
+    auxiliary_head=dict(num_classes=6),
+    test_cfg = dict(mode='slide',crop_size=(512, 512),  stride=(341, 341)))
+
+train_dataloader = dict(batch_size=8, num_workers=12)
+val_dataloader = dict(batch_size=1, num_workers=4)
+test_dataloader = val_dataloader
+
+vis_backends = [dict(type='LocalVisBackend')]
+visualizer = dict(
+    # test
+    type='SegLocalVisualizer', vis_backends=vis_backends, name='visualizer', alpha=1.0)
+# test
+default_hooks = dict(
+    logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
+    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=8000),
+    visualization=dict(type='SegVisualizationHook', draw=True, interval=1)
+)
