f3c8226ffcd145a53df5b4c27875e8c4669fd977
BinSpa <luogu1024116558@gmail.com>
2024-10-07 10:55:32 +0800

    changes

--- tools/test_command.py
+++ tools/test_command.py
@@ -1,7 +1,7 @@
 # mc_text
-bash torchrun_test.sh ../configs/mctextnet/segformer_urur.py ../../mmseg_exp/mctxt_urur/iter_112000.pth 1 --work-dir ../../mmseg_exp/mctxt_urur
+bash torchrun_test.sh ../configs/mctextnet/segformer_urur_txt.py ../../mmseg_exp/mctxt_urur/iter_112000.pth 1 --work-dir ../../mmseg_exp/mctxt_urur
 bash torchrun_test.sh ../configs/mctextnet/segformer_gid.py ../../mmseg_exp/mctxt_gid/iter_96000.pth 1 --work-dir ../../mmseg_exp/mctxt_gid
-bash torchrun_test.sh ../configs/mctextnet/segformer_fbp.py ../../mmseg_exp/mctxt_fbp/iter_96000.pth 1 --work-dir ../../mmseg_visual/FBP/mctxt
+bash torchrun_test.sh ../configs/mctextnet/segformer_fbp_txt.py ../../mmseg_exp/mctxt_fbp/iter_136000.pth 1 --work-dir ../../mmseg_visual/FBP/mctxt
 # baseline
 bash torchrun_test.sh ../configs/segformer/segformer-b5_fbp.py /home/rsr/gyl/RS_Code/Paper_Graph/Introduction/segformer_ckpts/fbp.pth 1 --work-dir ../../mmseg_exp/segformerb5_fbp
 bash torchrun_test.sh ../configs/segformer/segformer_mit-b5_8xb2-160k_gid-512x512.py /home/rsr/gyl/RS_Code/Paper_Graph/Introduction/segformer_ckpts/mc_gid.pth 1 --work-dir ../../mmseg_exp/segformerb5_gid
@@ -21,6 +21,7 @@ bash torchrun_test.sh ../configs/unet/unet_s5-d16_fcn_4xb4-160k_urur.py /data1/g
 # pspnet
 bash torchrun_test.sh ../configs/pspnet/pspnet_r101-d8_4xb4-80k_gid-512x512.py ../../mmseg_visual/pspnet_gid/iter_8000.pth 1 --work-dir ../../mmseg_visual/pspnet_gid
 bash torchrun_test.sh ../configs/pspnet/pspnet_r101-d8_4xb4-80k_urur-512x512.py ../../mmseg_visual/pspnet_urur/iter_80000.pth 1 --work-dir ../../mmseg_visual/pspnet_urur
+bash torchrun_test.sh ../configs/pspnet/pspnet_r101-d8_4xb4-80k_fbp-512x512.py ../../mmseg_exp/pspnet_fbp/iter_80000.pth 1 --work-dir ../../mmseg_visual/FBP/pspnet
 # deeplabv3+
 bash torchrun_test.sh ../configs/deeplabv3plus/deeplabv3plus_r101-d8_4xb4-160k_gid-512x512.py ../../mmseg_visual/deeplabv3p_gid/iter_16000.pth 1 --work-dir ../../mmseg_visual/deeplabv3p_gid
 bash torchrun_test.sh ../configs/deeplabv3plus/deeplabv3plus_r101-d8_4xb4-160k_urur-512x512.py ../../mmseg_visual/deeplabv3p_urur/iter_12000.pth 1 --work-dir ../../mmseg_visual/deeplabv3p_urur
